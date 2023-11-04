9ebbd4dfd65bc0a4e32448bb67586099f05e2c4f
Ria Cherry <lilyflowerangel@gmail.com>
2023-11-04 20:26:18 +0530

    Add project files.

---
+++ CSX.Toolkits.DotNet/Filtering/NullabilityExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace CSX.Toolkits.DotNet.Filtering;
+
+public static class NullabilityExtensions
+{
+    public static IEnumerable<T> WhereNotNull<T>(this IEnumerable<T?> e)
+    {
+        foreach (var item in e)
+        {
+            if (item is T t)
+                yield return t;
+        }
+    }
+}
---
+++ CSX.Toolkits.DotNet/Mathematics/Clamp.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSX.Toolkits.DotNet.Mathematics;
+
+public static partial class Clamp
+{
+    // As Byte
+
+    public static byte ClampAsByte(this int value)
+        => (byte)Math.Clamp(value, 0, 255);
+
+    public static byte ClampAsByte(this short value)
+        => (byte)Math.Clamp(value, (short)0, (short)255);
+
+    public static byte ClampAsByte(this long value)
+        => (byte)Math.Clamp(value, 0, 255);
+
+    public static byte ClampAsByte(this float value)
+        => (byte)Math.Clamp(value, 0, 255);
+
+    public static byte ClampAsByte(this double value)
+        => (byte)Math.Clamp(value, 0, 255);
+
+    public static byte ClampAsByte(this decimal value)
+        => (byte)Math.Clamp(value, 0, 255);
+}
